cd289dcb08872a6dd377f06f0aa8ecc30950435d
Thalia <97253671+userThaliaoliveira@users.noreply.github.com>
2024-08-06 09:32:28 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "290696Th@",
+    password: "thaliadev",
     database: "crud"
 })
